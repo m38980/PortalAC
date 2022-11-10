fe737e85bd55de44d210d5b1cba2424a1cc341bc
unknown <Admin@wks-ee03.bcstp.local>
2022-11-10 09:56:22 +0000

    first commit

---
+++ PortalConsumidor/Controllers/TesteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortalConsumidor.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortalConsumidor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PortalConsumidor.MvcApplication" Language="C#" %>
